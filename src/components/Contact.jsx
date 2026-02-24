fc501b1736d7c886e63d04593b80d460fc4a77e5
jcarri07 <carrizalesj5@gmail.com>
2026-02-24 15:21:22 -0400

    little fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -103,7 +103,7 @@ export default function Contact() {
                         href="https://www.linkedin.com/in/jos%C3%A9-carrizales-b31b71186/"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="btn-outline-glow"
+                        className="btn-outline-glow contact__cta-btn"
                     >
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
                             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
